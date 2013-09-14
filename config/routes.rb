d5be66055f7caf8a1ee44a48b0a6e72c6200b589
Jason Stebbing <jstebbing@hbpthree.com>
2013-09-14 02:03:23 -0600

    Add jQuery Masonry

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Omrails::Application.routes.draw do
   resources :pins
-
-
   devise_for :users
-
   get 'about' => 'pages#about'
-
   root :to => 'pages#home'
   
   # The priority is based upon order of creation:
